ae7ae87f6199bb22524bd69f458feaa2bb00e68b
samfdl <samfdl@163.com>
2018-07-19 10:56:40 +0800

    。。。

--- app/src/main/java/com/samfdl/demo/custom/BottomUpActivity.java
+++ app/src/main/java/com/samfdl/demo/custom/BottomUpActivity.java
@@ -2,13 +2,26 @@ package com.samfdl.demo.custom;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.samfdl.demo.R;
 
-public class BottomUpActivity extends AppCompatActivity {
+public class BottomUpActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView publish;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_bottomup);
+
+        publish = findViewById(R.id.publish);
+        publish.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this, "onClick ", Toast.LENGTH_SHORT).show();
     }
 }
