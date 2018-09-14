e24264d143db07ebb9ba0373f439af98610af452
冯德林 <samfdl@163.com>
2018-09-14 09:43:05 +0800

    添加 jsbridge

--- app/src/main/java/com/samfdl/demo/lib/JsBridgeActivity.java
+++ app/src/main/java/com/samfdl/demo/lib/JsBridgeActivity.java
@@ -1,14 +1,71 @@
 package com.samfdl.demo.lib;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.widget.Button;
 
+import com.github.lzyzsd.jsbridge.BridgeHandler;
+import com.github.lzyzsd.jsbridge.BridgeWebView;
+import com.github.lzyzsd.jsbridge.CallBackFunction;
+import com.github.lzyzsd.jsbridge.DefaultHandler;
 import com.samfdl.demo.R;
 
-public class JsBridgeActivity extends AppCompatActivity {
+public class JsBridgeActivity extends AppCompatActivity implements View.OnClickListener {
+    private final String TAG = "JsBridgeActivity";
+
+    BridgeWebView webView;
+
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lib_jsbridge);
+
+        webView = findViewById(R.id.webView);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(this);
+
+        webView.setDefaultHandler(new DefaultHandler());
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @SuppressWarnings("unused")
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType, String capture) {
+                this.openFileChooser(uploadMsg);
+            }
+
+            @SuppressWarnings("unused")
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType) {
+                this.openFileChooser(uploadMsg);
+            }
+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+//                mUploadMessage = uploadMsg;
+//                pickFile();
+            }
+        });
+
+//        webView.loadUrl("file:///android_asset/demo.html");
+        webView.loadUrl("http://tst.candy.one/jsbridge");
+
+        webView.registerHandler("submitFromWeb", new BridgeHandler() {
+            @Override
+            public void handler(String data, CallBackFunction function) {
+                Log.i(TAG, "handler = submitFromWeb, data from web = " + data);
+                function.onCallBack("submitFromWeb exe, response data 中文 from Java");
+            }
+
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
     }
 }
